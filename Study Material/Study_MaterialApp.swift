c74aa287d3a82617e4e9a0dd40c69da728194e66
sanjeevan43 <sanjeevamoorthy35@gmail.com>
2025-01-23 18:47:52 +0530

    Initial Commit

---
+++ Study Material/Study_MaterialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_MaterialApp.swift
+//  Study Material
+//
+//  Created by sanjeevan on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Study_MaterialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
