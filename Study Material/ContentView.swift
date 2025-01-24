f97451d6567c061b711175becbad10e28e766813
sanjeevan43 <sanjeevamoorthy35@gmail.com>
2025-01-24 14:26:53 +0530

    new changes

--- Study Material/ContentView.swift
+++ Study Material/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("SANJEEVAN")
-        }
-        .padding()
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(0..<10) { _ in
+                        VStack {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 20) {
+                                    ForEach(0..<10) { _ in
+                                        VStack {
+                                            Image("fedhar")
+                                                .resizable()
+                                                .frame(width: 100, height: 100)
+                                                .clipShape(Circle())
+                                        }
+                                    }
+                                }
+                            }
+                            .padding()
+                        }
+                        VStack {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 20) {
+                                    ForEach(0..<10) { _ in
+                                        VStack {
+                                            Image("blode bard")
+                                                .resizable()
+                                                .frame(width: 100, height: 100)
+                                                .clipShape(Circle())
+                                        }
+                                    }
+                                }
+                            }
+                            .padding()
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)         }
     }
 }
 
---
+++ Study Material/Image_scroll.swift
@@ -0,0 +1,18 @@
+//
+//  Image_scroll.swift
+//  Study Material
+//
+//  Created by sanjeevan on 24/01/25.
+//
+
+import SwiftUI
+
+struct Image_scroll: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Image_scroll()
+}
